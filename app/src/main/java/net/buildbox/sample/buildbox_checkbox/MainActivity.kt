82200de4a69b47ca5594031f0a8b754c0f839873
Koji Shigemura <shige0501@gmail.com>
2019-09-30 22:01:35 +0900

    チェックボックスの処理追加

--- app/src/main/java/net/buildbox/sample/buildbox_checkbox/MainActivity.kt
+++ app/src/main/java/net/buildbox/sample/buildbox_checkbox/MainActivity.kt
@@ -2,11 +2,19 @@ package net.buildbox.sample.buildbox_checkbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sampleCheckBox: CheckBox = findViewById(R.id.sample_checkbox)
+        sampleCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            val stateMessage = if (isChecked) "選択されました" else  "選択解除されました"
+            Toast.makeText(this, "チェックボックスが $stateMessage", Toast.LENGTH_SHORT).show()
+        }
     }
 }
